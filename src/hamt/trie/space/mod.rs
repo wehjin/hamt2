83a924b61d4e78ed61ed09e3cc7222163a70d49e
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2026-03-19 09:22:22 -0700

    Removes struct MemTrie.

--- src/hamt/trie/space/mod.rs
+++ src/hamt/trie/space/mod.rs
@@ -1,41 +1,36 @@
 use crate::client::{QueryError, TransactError};
 use crate::hamt::trie::key::TrieKey;
+use crate::hamt::trie::map_base::TrieMapBase;
 use crate::hamt::trie::mem::value::MemValue;
-use crate::hamt::trie::mem::MemTrie;
 use crate::hamt::trie::value::TrieValue;
 
 pub mod core;
 
 #[derive(Debug)]
 pub struct SpaceTrie {
-    mem_trie: Option<MemTrie>,
+    map_base: TrieMapBase,
 }
 
 impl SpaceTrie {
     pub fn new() -> Self {
-        Self { mem_trie: None }
+        Self {
+            map_base: TrieMapBase::empty(),
+        }
     }
 
     pub fn insert(self, key: i32, value: MemValue) -> Result<Self, TransactError> {
         let key = TrieKey::new(key);
         let value = TrieValue::Mem(value);
-        let mem_trie = if let Some(mem_trie) = self.mem_trie {
-            mem_trie.insert(key, value)?
-        } else {
-            MemTrie::one_kv(key, value)?
-        };
-        Ok(Self {
-            mem_trie: Some(mem_trie),
-        })
+        let map_base = self.map_base.insert_kv(key, value)?;
+        Ok(Self { map_base })
     }
 
     pub fn query_value(&self, key: i32) -> Result<Option<MemValue>, QueryError> {
         let key = TrieKey::new(key);
-        if let Some(mem_trie) = &self.mem_trie {
-            let value = mem_trie.query_value(key)?.map(|TrieValue::Mem(v)| v);
-            Ok(value)
-        } else {
-            Ok(None)
-        }
+        let value = self
+            .map_base
+            .query_value(key)?
+            .map(|TrieValue::Mem(value)| value);
+        Ok(value)
     }
 }
