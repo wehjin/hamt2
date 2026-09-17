acd9951efd0c969f255edbc163d968996eee57c2
wehjin <92849+wehjin@users.noreply.github.com>
2026-09-17 07:01:46 -0700

    Messing around with memos.

--- crates/skybase/src/routes/wss_sandbox.rs
+++ crates/skybase/src/routes/wss_sandbox.rs
@@ -1,9 +1,35 @@
 use codee::string::FromToStringCodec;
+use leptos::logging::error;
 use leptos::prelude::*;
-use leptos_use::{use_websocket, UseWebSocketReturn};
+use leptos_use::{UseWebSocketReturn, use_websocket};
 use sky_server::shared::{SocketRequest, SocketResponse};
-use sky_types::db::{Attr, datom, val};
+use sky_types::db::{Attr, Datom, datom, val};
 use sky_types::trie::SlotBaseId;
+use std::sync::Arc;
+
+#[derive(Clone)]
+pub struct RequestSender {
+    send: Arc<dyn Fn(&String)>,
+}
+
+impl RequestSender {
+    pub fn new(send: Arc<dyn Fn(&String)>) -> Self {
+        Self { send }
+    }
+    fn send_request(&self, request: SocketRequest) {
+        let message = serde_json::to_string(&request).expect("serialize request");
+        (self.send)(&message)
+    }
+    pub fn send_connect(&self) {
+        self.send_request(SocketRequest::Connect);
+    }
+    pub fn send_read(&self, id: SlotBaseId) {
+        self.send_request(SocketRequest::ReadSlotBase(id));
+    }
+    pub fn send_transact(&self, datoms: impl Into<Vec<Datom>>) {
+        self.send_request(SocketRequest::Transact(datoms.into()));
+    }
+}
 
 #[component]
 pub fn WebSocketSandbox() -> impl IntoView {
@@ -13,56 +39,53 @@ pub fn WebSocketSandbox() -> impl IntoView {
         send,
         ..
     } = use_websocket::<String, String, FromToStringCodec>("/ws");
-
-    let max_id = RwSignal::new(None::<SlotBaseId>);
-    let last_response = RwSignal::new(String::new());
-
-    Effect::new(move || {
-        let Some(json) = message.get() else {
-            return;
-        };
-        match serde_json::from_str::<SocketResponse>(&json) {
-            Ok(response) => {
-                last_response.set(
-                    serde_json::to_string_pretty(&response).expect("serialize response"),
-                );
-                match response {
-                    SocketResponse::StorageStatus(head)
-                    | SocketResponse::TransactResult(head) => {
-                        max_id.set(Some(head.max_id));
-                    }
-                    _ => {}
+    let request_sender = RequestSender::new(Arc::new(send.clone()));
+    let received_response = Memo::new(move |_| match message.get() {
+        None => None,
+        Some(json) => {
+            let result = serde_json::from_str::<SocketResponse>(&json);
+            match result {
+                Ok(response) => Some(response),
+                Err(e) => {
+                    error!("message parse error: {:?}", e);
+                    None
                 }
             }
-            Err(e) => last_response.set(format!("unexpected message: {json:?} ({e})")),
         }
     });
-
-    let send_request = {
-        let send = send.clone();
-        move |request: SocketRequest| {
-            send(&serde_json::to_string(&request).expect("serialize request"));
-        }
-    };
-
+    let max_id = Memo::new(move |_| match received_response.get() {
+        Some(response) => match response {
+            SocketResponse::StorageStatus(head) | SocketResponse::TransactResult(head) => {
+                Some(head.max_id)
+            }
+            _ => None,
+        },
+        None => None,
+    });
+    let last_response = Memo::new(move |_| {
+        received_response
+            .get()
+            .map(|response| serde_json::to_string_pretty(&response).expect("serialize response"))
+    });
     let send_connect = {
-        let send_request = send_request.clone();
-        move |_| send_request(SocketRequest::Connect)
+        let sender = request_sender.clone();
+        move |_| sender.send_connect()
     };
     let send_read_slot_base = {
-        let send_request = send_request.clone();
+        let sender = request_sender.clone();
         move |_| {
             if let Some(id) = max_id.get() {
-                send_request(SocketRequest::ReadSlotBase(id));
+                sender.send_read(id);
             }
         }
     };
-    let send_transact = move |_| {
-        send_request(SocketRequest::Transact(vec![datom::add(
-            100,
-            Attr::from("skybase/version"),
-            val("0.1"),
-        )]));
+
+    let send_transact = {
+        let sender = request_sender.clone();
+        move |_| {
+            let datoms = vec![datom::add(100, Attr::from("skybase/version"), val("0.1"))];
+            sender.send_transact(datoms);
+        }
     };
 
     view! {
