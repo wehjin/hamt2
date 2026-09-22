b012ad3932aa3f7c2ba2394b4b8f610fc1e2ebc5
wehjin <92849+wehjin@users.noreply.github.com>
2026-09-22 16:31:31 -0700

    Implements TrieStream for MemStorage.

--- crates/sky-types/src/storage/mem/mem_trie_mut.rs
+++ crates/sky-types/src/storage/mem/mem_trie_mut.rs
@@ -2,7 +2,7 @@ use crate::storage::{
     MemReadStorage, ReadStorage, ReadStorageError, ReadWriteStorage, StorageStatus,
     WriteStorageError,
 };
-use crate::trie::{Base, BaseId, MapBase, RootBaseRead};
+use crate::trie::{Base, BaseId, MapBase, RootBaseRead, TrieStream};
 
 #[derive(Debug)]
 pub struct MemStorage {
@@ -13,6 +13,14 @@ impl MemStorage {
     pub fn new() -> Self {
         let inner = MemReadStorage::empty();
         Self { inner }
+    }
+}
+
+impl TrieStream for MemStorage {
+    type Subtrie = MemReadStorage;
+
+    fn to_subtrie(&self, subtrie_root: MapBase) -> Self::Subtrie {
+        self.inner.to_subtrie(subtrie_root)
     }
 }
 
@@ -60,5 +68,23 @@ impl RootBaseRead for MemStorage {
 
     fn read_root(&self) -> MapBase {
         self.inner.read_root()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::storage::{MemStorage, ReadWriteStorage};
+    use crate::trie::{HashKey, TrieStream, TrieValue, map_base};
+    use futures::StreamExt;
+
+    #[tokio::test]
+    async fn mem_trie_mut_works() {
+        let mut m = MemStorage::new();
+        let map_base = map_base::one_kv(HashKey::new(27), TrieValue::U32(28), &mut m)
+            .await
+            .unwrap();
+        m.write_root(map_base).await.unwrap();
+        let values = m.u32_stream().collect::<Vec<_>>().await;
+        assert_eq!(values, vec![(27, 28)]);
     }
 }
